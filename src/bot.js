e901848a5707a7aaa26e7e1a052b9e178a3d553b
Lucah Lucchesi <lucahlucchesi@gmail.com>
2025-03-05 15:40:08 -0600

    Added cooldown to start command

--- src/bot.js
+++ src/bot.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 const client = new Client({
     intents: [GatewayIntentBits.Guilds]
 });
+// Cooldown time in milliseconds
+const cooldownLength = 5 * 60 * 1000;
+// Stores when the cooldown is over
+let cooldownEndtime = 0;
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}.`);
-})
 
 // Handles commands
 client.on('interactionCreate', (interaction) => {
@@ -17,12 +18,26 @@ client.on('interactionCreate', (interaction) => {
     }
     // Server start script here
     if (interaction.commandName == 'start') {
-        interaction.reply("Server started");
+        const currentTime = Date.now()
+        if(currentTime < cooldownEndtime) {
+            interaction.reply(`The server was recently started. Please wait ${Math.ceil((cooldownEndtime - currentTime) / 1000)} seconds.`);
+        } else {
+            cooldownEndtime = currentTime + cooldownLength;
+            interaction.reply("Server started");
+        }
     }
     // Server stop script here
     if (interaction.commandName == 'stop') {
         interaction.reply("Server stopped");
     }
+})
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}.`);
+    client.user.setActivity({
+        name: 'Minecraft',
+        type: ActivityType.Playing
+    })
 })
 
 client.login(process.env.TOKEN);
