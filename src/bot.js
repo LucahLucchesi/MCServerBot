4ecb291b58b31513a9b250f8e886a8ee90ec5a01
Lucah Lucchesi <lucahlucchesi@gmail.com>
2025-03-05 01:30:40 -0600

    Basic command functionality

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,28 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({
-    intents: [GatewayIntentBits.GuildMessages]
+    intents: [GatewayIntentBits.Guilds]
 });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}.`);
+})
+
+// Handles commands
+client.on('interactionCreate', (interaction) => {
+    if (!interaction.isChatInputCommand()) { return; }
+    
+    if (interaction.commandName == 'ping') {
+        interaction.reply("pong!");
+    }
+    // Server start script here
+    if (interaction.commandName == 'start') {
+        interaction.reply("Server started");
+    }
+    // Server stop script here
+    if (interaction.commandName == 'stop') {
+        interaction.reply("Server stopped");
+    }
+})
 
 client.login(process.env.TOKEN);
